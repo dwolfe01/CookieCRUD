8a1d6bdbf9b32486f5ad25e71c9378e68e219d49
Wolfe <dwolfe@wileyseicesmbp2.home>
2015-06-10 18:55:54 +0100

    Integration tests

---
+++ src/test/java/wolfesoftware/integration/controller/SailFishCookieCrudIntegrationTest.java
@@ -0,0 +1,25 @@
+package wolfesoftware.integration.controller;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.springframework.util.ResourceUtils;
+
+import com.wolfesoftware.sailfish.concurrency.ReadySteadyThread;
+import com.wolfesoftware.sailfish.concurrency.worker.factory.HttpUserWorkerFactoryFromJSONFile;
+import com.wolfesoftware.sailfish.responsehandler.factory.ResponseHandlerFactory;
+import com.wolfesoftware.sailfish.responsehandler.factory.ResponseHandlerFactory.ResponseHandlers;
+
+public class SailFishCookieCrudIntegrationTest {
+
+	@Test
+	public void shouldCookieCrud() throws Exception {
+		HttpUserWorkerFactoryFromJSONFile factory = new HttpUserWorkerFactoryFromJSONFile();
+		String jsonHttpUser = FileUtils.readFileToString(ResourceUtils.getFile("classpath:sailfish/cookieCrud.json"));
+		//FileOutputStream fos = new FileOutputStream(new File("/tmp/output.html"));
+		//ResponseHandlers.OUTPUTSTREAM.setOutputStream(fos);
+		ResponseHandlerFactory.setHandler(ResponseHandlers.OUTPUTSTREAM);
+		factory.setJSON(jsonHttpUser);
+		new ReadySteadyThread(10, factory).go();
+	}
+
+}
